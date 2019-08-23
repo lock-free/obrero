01be798498249ef4d7baa023069f7469d2eed071
loveKino <842914439@qq.com>
2019-08-24 00:26:41 +0800

    add ParseArgs help function

--- obrero.go
+++ obrero.go
@@ -3,6 +3,7 @@ package obrero
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/lock-free/gopcp"
 	"github.com/lock-free/gopcp_rpc"
 	"github.com/lock-free/gopcp_stream"
@@ -179,4 +180,27 @@ func ReadJson(filePath string, f interface{}) error {
 		return err
 	}
 	return json.Unmarshal([]byte(source), f)
+}
+
+// parse args and assign values to pointers
+func ParseArgs(args []interface{}, ps []interface{}, errMsg string) error {
+	if len(args) < len(ps) {
+		return fmt.Errorf("missing some args, args=%v, %s", args, errMsg)
+	}
+
+	for i, p := range ps {
+		err := ParseArg(args[i], p)
+		if err != nil {
+			return fmt.Errorf("fail to parse arg at %d, args=%v, %s", i, args, errMsg)
+		}
+	}
+	return nil
+}
+
+func ParseArg(arg interface{}, pointer interface{}) error {
+	bs, err := json.Marshal(arg)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bs, pointer)
 }
