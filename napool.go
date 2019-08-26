f8317a07a4d82242b1c60be4b86d9baa7518a46c
loveKino <842914439@qq.com>
2019-08-26 23:02:13 +0800

    add hashNA function

--- napool.go
+++ napool.go
@@ -16,7 +16,7 @@ type NAPools struct {
 }
 
 func (naPools *NAPools) CallProxy(serviceType string, list gopcp.CallResult, timeout time.Duration) (interface{}, error) {
-	client, err := naPools.GetItem()
+	client, err := naPools.GetRandomItem()
 
 	if err != nil {
 		return nil, err
@@ -26,7 +26,7 @@ func (naPools *NAPools) CallProxy(serviceType string, list gopcp.CallResult, tim
 }
 
 func (naPools *NAPools) CallProxyById(serviceId string, serviceType string, list gopcp.CallResult, timeout time.Duration) (interface{}, error) {
-	client, err := naPools.GetItem()
+	client, err := naPools.GetRandomItem()
 
 	if err != nil {
 		return nil, err
@@ -36,7 +36,7 @@ func (naPools *NAPools) CallProxyById(serviceId string, serviceType string, list
 }
 
 func (naPools *NAPools) CallProxyStream(serviceType string, list gopcp.CallResult, streamCallback gopcp_stream.StreamCallbackFunc, timeout time.Duration) (interface{}, error) {
-	client, err := naPools.GetItem()
+	client, err := naPools.GetRandomItem()
 
 	if err != nil {
 		return nil, err
@@ -50,26 +50,57 @@ func (naPools *NAPools) CallProxyStream(serviceType string, list gopcp.CallResul
 	return client.Call(*sexp, timeout)
 }
 
-func (naPools *NAPools) GetItem() (*gopcp_rpc.PCPConnectionHandler, error) {
-	return naPools.getItem(0, naPools.GetClientMaxRetry)
+// pick up a random item
+func (naPools *NAPools) GetRandomItem() (*gopcp_rpc.PCPConnectionHandler, error) {
+	return naPools.getRandomItem(0, naPools.GetClientMaxRetry)
 }
 
-func (naPools *NAPools) getItem(tryCount int, maxCount int) (*gopcp_rpc.PCPConnectionHandler, error) {
+func (naPools *NAPools) hashNA(key []byte) (*gopcp_rpc.PCPConnectionHandler, error) {
+	index := getHash(key) % len(naPools.Pools)
+
+	item, err := naPools.Pools[index].Get()
+	if err != nil {
+		return nil, err
+	}
+
+	client, ok := item.(*gopcp_rpc.PCPConnectionHandler)
+	if !ok {
+		return nil, errors.New("unexpected error at hashNA")
+	} else {
+		return client, nil
+	}
+}
+
+func getHash(data []byte) int {
+	var s uint32 = 2166136261
+
+	// write data
+	for _, c := range data {
+		s *= 16777619
+		s ^= uint32(c)
+	}
+
+	return int(s)
+}
+
+// TODO implement robin-round instead of random
+func (naPools *NAPools) getRandomItem(tryCount int, maxCount int) (*gopcp_rpc.PCPConnectionHandler, error) {
 	if tryCount > maxCount {
 		return nil, errors.New("fail to get a connection from NA pools, tried 3 times")
 	}
 
+	// pick up a random na pool.
 	index := rand.Intn(len(naPools.Pools))
 
 	item, err := naPools.Pools[index].Get()
 
 	if err != nil {
-		return naPools.getItem(tryCount+1, maxCount)
+		return naPools.getRandomItem(tryCount+1, maxCount)
 	} else {
 		client, ok := item.(*gopcp_rpc.PCPConnectionHandler)
 		if !ok {
 			// TODO sleep a while before retry
-			return naPools.getItem(tryCount+1, maxCount)
+			return naPools.getRandomItem(tryCount+1, maxCount)
 		} else {
 			return client, nil
 		}
