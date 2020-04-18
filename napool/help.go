1d6c995ce8c5c5fa54ff9d1f9706327d495422e4
loveKino <842914439@qq.com>
2020-04-18 22:49:57 +0800

    opt

--- napool/help.go
+++ napool/help.go
@@ -8,5 +8,5 @@ import (
 var pcpClient = gopcp.PcpClient{}
 
 func (naPools *NAPools) SimpleCall(serviceType string, methodName string, args ...interface{}) (interface{}, error) {
-	return naPools.CallProxy(serviceType, pcpClient.Call(methodName, args), 120*time.Second)
+	return naPools.CallProxy(serviceType, pcpClient.Call(methodName, args...), 120*time.Second)
 }
